48158f5b9e096896e01c3405f1122db352bf2251
shuxiny9 <shuxiny9@gmail.com>
2025-07-07 10:39:53 -0700

    installed StatusCodes library and created Custom Erro Class for more specific erro messages

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,46 +1,36 @@
 import Job from '../models/JobModel.js';
-
-import { nanoid } from 'nanoid';
-
-
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end developer' },
-  { id: nanoid(), company: 'google', position: 'back-end developer' },
-];
-
+import { StatusCodes } from 'http-status-codes';
+import { NotFoundError } from '../errors/customErrors.js';
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
-  res.status(200).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
   //const { company, position } = req.body;
   const job = await Job.create( req.body );
-  res.status(201).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  res.status(200).json({ job });
+  console.log(job);
+  if (!job) throw new NotFoundError(`no job with id ${id} for getting job`);
+  res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
   const { id } = req.params;
-
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
     new: true,
   });
 
   if (!updatedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    return res.status(404).json({ msg: `no job with id ${id} for updating` });
   }
-
-  res.status(200).json({ job: updatedJob });
+  res.status(StatusCodes.OK).json({ job: updatedJob });
 };
 
 
@@ -49,7 +39,7 @@ export const deleteJob = async (req, res) => {
   const removedJob = await Job.findByIdAndDelete(id);
 
   if (!removedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    return res.status(404).json({ msg: `no job with id ${id} for deletion` });
   }
-  res.status(200).json({ job: removedJob });
+  res.status(StatusCodes.OK).json({ job: removedJob });
 };

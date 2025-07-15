0b6be9106878facc8990f3a31aca9cdbb444bfd6
shuxiny9 <shuxiny9@gmail.com>
2025-07-15 14:07:41 -0700

    mplemented AdminPage for admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,8 @@ import { action as addJobAction } from './pages/AddJob';
 import { loader as allJobsLoader } from './pages/AllJobs';
 import { loader as editJobLoader } from './pages/EditJob';
 import { action as editJobAction } from './pages/EditJob';
+import { action as deleteJobAction } from './pages/DeleteJob';
+import { loader as adminLoader } from './pages/Admin';
 
 
 const checkDefaultTheme = () => {
@@ -79,12 +81,17 @@ const router = createBrowserRouter([
           {
             path: 'admin',
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: 'edit-job/:id',
             element: <EditJob />,
             loader: editJobLoader,
             action: editJobAction,
+          },
+          {
+            path: 'delete-job/:id',
+            action: deleteJobAction
           },
         ],
       },
--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { redirect } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import { toast } from '../utils/toast'; // toast.js
 
-const DeleteJob = () => {
-  return (
-    <div>DeleteJob</div>
-  )
+export async function action({ params }) {
+  try {
+    await customFetch.delete(`/jobs/${params.id}`);
+    toast.success('Job deleted successfully');
+  } catch (error) {
+    toast.error(error.response.data.msg);
+  }
+  return redirect('/dashboard/all-jobs');
 }
-
-export default DeleteJob

354ad7feb9d4d3b5fe710e2398e3bddbb1a5d987
shuxiny9 <shuxiny9@gmail.com>
2025-07-14 19:06:51 -0700

    set up logout functionality in dashboardPage

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,8 +1,9 @@
-import { Outlet, redirect, useLoaderData } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData,useNavigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { Navbar, BigSidebar, SmallSidebar } from '../components';
 import { useState, createContext, useContext } from 'react';
 import customFetch from '../utils/customFetch';
+import { toast } from '../utils/toast'; // toast.js
 
 export const loader = async () => {
   try {
@@ -18,6 +19,7 @@ const DashboardContext = createContext();
 const Dashboard = ({ isDarkThemeEnabled }) => {
 
   const { user } = useLoaderData();
+  const navigate = useNavigate();
 
   const [showSidebar, setShowSidebar] = useState(false);
   //const [isDarkTheme, setIsDarkTheme] = useState(true);
@@ -35,7 +37,9 @@ const Dashboard = ({ isDarkThemeEnabled }) => {
   };
 
   const logoutUser = async () => {
-    console.log('logout user');
+    navigate('/');
+    await customFetch.get('/auth/logout');
+    toast.success('Logging out...');
   };
 
   return (

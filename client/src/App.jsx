3d00685a5487f2667c017d911715cc0fc4dd69e6
shuxiny9 <shuxiny9@gmail.com>
2025-07-18 11:06:32 -0700

    added Stats Container , ChartsContainer before install recharter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ import { action as editJobAction } from './pages/EditJob';
 import { action as deleteJobAction } from './pages/DeleteJob';
 import { loader as adminLoader } from './pages/Admin';
 import { action as profileAction } from './pages/Profile';
+import { loader as statsLoader } from './pages/Stats';
 
 
 const checkDefaultTheme = () => {
@@ -67,7 +68,8 @@ const router = createBrowserRouter([
           },
           {
             path: 'stats',
-            element: <Stats />
+            element: <Stats />,
+            loader: statsLoader,
           },
           {
             path: 'all-jobs',
---
+++ client/src/components/AreaChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AreaChart = () => {
+  return (
+    <h2>AreaChart</h2>
+  )
+}
+
+export default AreaChart

3ffe6406783edb1a2d543aaeae49a658f7ffd6fb
shuxiny9 <shuxiny9@gmail.com>
2025-07-21 12:37:09 -0700

    fixed front end

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -55,7 +56,7 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname,  './client/dist',  'index.html'));
 });
 
 

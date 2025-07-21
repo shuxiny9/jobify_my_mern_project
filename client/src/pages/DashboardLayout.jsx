7788e3c7a24d0ff776b456cdc77541af03f0767e
shuxiny9 <shuxiny9@gmail.com>
2025-07-21 13:23:46 -0700

    before set up Reat Query, saving codes

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,9 +1,11 @@
-import { Outlet, redirect, useLoaderData,useNavigate } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
-import { Navbar, BigSidebar, SmallSidebar } from '../components';
+import { Navbar, BigSidebar, SmallSidebar, Loading } from '../components';
 import { useState, createContext, useContext } from 'react';
 import customFetch from '../utils/customFetch';
-import { toast } from '../utils/toast'; // toast.js
+import { toast } from '../utils/toast'; // toast.
+import { useNavigation } from 'react-router-dom';
+import { Loading } from '../components';
 
 export const loader = async () => {
   try {
@@ -19,6 +21,9 @@ const DashboardContext = createContext();
 const Dashboard = ({ isDarkThemeEnabled }) => {
 
   const { user } = useLoaderData();
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === 'loading';
+
   const navigate = useNavigate();
 
   const [showSidebar, setShowSidebar] = useState(false);
@@ -60,7 +65,7 @@ const Dashboard = ({ isDarkThemeEnabled }) => {
           <div>
             <Navbar />
             <div className='dashboard-page'>
-              <Outlet context={{ user }} />
+               {isPageLoading ? <Loading /> : <Outlet context={{ user }} />}
             </div>
           </div>
         </main>
